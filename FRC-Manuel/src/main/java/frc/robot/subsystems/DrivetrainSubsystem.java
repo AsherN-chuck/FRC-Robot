d641ed4e0335d759fd1fc558ad95f3cd6a8403df
AsherN-chuck <NieffAsh000@students.wvsd.org>
2020-07-14 15:03:19 -0700

    Added DrivetrainSubsystem

--- FRC-Manuel/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ FRC-Manuel/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,3 +1,10 @@
-public class DrivetrainSubsystem {
-    
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+    private final SpeedController leftmoter = new Spark(0);
+    private final SpeedController rightmoter = new Spark(1);
 }
